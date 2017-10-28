ee71ba886d9b5b61a94ad5ae98cf2cce2a9fd7aa
julianschuler <31921487+julianschuler@users.noreply.github.com>
2017-10-28 23:28:55 +0200

    Moved looping file to hardware

--- background-sound/background-sound.ino
+++ background-sound/background-sound.ino
@@ -1,15 +1,12 @@
 #include <SoftwareSerial.h>
 
-#define Mp3Module1Pin		A1		// connect it over 1kOhm with the RX pin of the first DFPlayerMini
-#define Mp3Module2Pin		A5		// connect it over 1kOhm with the RX pin of the second DFPlayerMini
-#define VolumeModule1		30		// max: 30
-#define VolumeModule2		30		// max: 30
+#define Mp3ModulePin		A5		// connect it over 1kOhm with the RX pin of the DFPlayerMini
+#define Volume				30		// max: 30
 #define DebounceTime		20
 
-SoftwareSerial Mp3Module1(255, Mp3Module1Pin);
-SoftwareSerial Mp3Module2(255, Mp3Module2Pin);
+SoftwareSerial Mp3Module(255, Mp3ModulePin);
 
-const uint8_t ButtonPins[] = {7, 8, 9, 10, 11, 12};
+const uint8_t ButtonPins[] = {2, 3, 4, 5, 6, 7};
 bool lastState[sizeof(ButtonPins)];
 
 
@@ -17,11 +14,8 @@ void setup() {
 	for (uint8_t i = 0; i < sizeof(ButtonPins); i++) {
 		pinMode(ButtonPins[i], INPUT_PULLUP);
 	}
-	Mp3Module1.begin(9600);
-	Mp3Module2.begin(9600);
-	SetVolume(&Mp3Module1, VolumeModule1);
-	SetVolume(&Mp3Module2, VolumeModule2);
-	LoopFile(&Mp3Module1, 1);
+	Mp3Module.begin(9600);
+	SetVolume(Volume);
 }
 
 
@@ -30,7 +24,7 @@ void loop() {
 		if (digitalRead(ButtonPins[i]) == LOW) {
 			if (lastState[i] == HIGH) {
 				lastState[i] = LOW;
-				PlayFile(&Mp3Module2, i + 1);
+				PlayFile(i + 1);
 				delay(DebounceTime);
 			}
 		}
@@ -42,24 +36,19 @@ void loop() {
 }
 
 
-void SendCommand(Stream* _Serial, uint8_t cmd, uint8_t paraml=0x00, uint8_t paramh=0x00) {
+void SendCommand(uint8_t cmd, uint8_t paraml=0x00, uint8_t paramh=0x00) {
 	uint8_t buf[] = {0x7E, 0xFF, 0x06, cmd, 0x00, paramh, paraml, 0xEF};
 	for (uint8_t i = 0; i < 8; i++) {
-		_Serial->write(buf[i]);
+		Mp3Module.write(buf[i]);
 	}
 }
 
 
-void SetVolume(Stream* _Serial, uint8_t volume) {
-	SendCommand(_Serial, 0x10, volume, 0x01);
+void SetVolume(uint8_t volume) {
+	SendCommand(0x10, volume, 0x01);
 }
 
 
-void LoopFile(Stream* _Serial, uint8_t file) {
-	SendCommand(_Serial, 0x08, file, 0);
-}
-
-
-void PlayFile(Stream* _Serial, uint8_t file){
-	SendCommand(_Serial, 0x03, file, 0);
+void PlayFile(uint8_t file){
+	SendCommand(0x03, file, 0);
 }
